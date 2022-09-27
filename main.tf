1420e6a905c3f3d9ae9205b50b7bd2827d80399e
Leandro Monteiro <105895451+lmonteirof@users.noreply.github.com>
2022-09-26 21:51:31 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,62 +1,20 @@
-data "aws_vpc" "catapimba_vpc" {
-  filter {
-    name   = "tag:Name"
-    values = ["catapimba-corps-vpc"]
-  }
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
+  name = "catapimba-corps-vpc"
+  cidr = "10.0.0.0/16"
 
-  filter {
-    name = "name"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-    values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
-    ]
-  }
-
-  filter {
-    name = "owner-alias"
-
-    values = [
-      "amazon",
-    ]
-  }
-}
-
-module "jenkins_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "jenkins-sg"
-  description = "Security group para o servidor do Jenkins Server"
-  vpc_id      = data.aws_vpc.catapimba_vpc.id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "ssh-tcp"]
-  egress_rules        = ["all-all"]
-}
-
-
-module "jenkins_ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name                   = "Jenkins-Server"
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  key_name               = "vockey"
-  monitoring             = true
-  vpc_security_group_ids = [module.jenkins_sg.security_group_id]
-  subnet_id              = "subnet-0c51bc26a0d853d44"
-  iam_instance_profile   = "LabInstanceProfile"
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
+    Environment = "prod"
+    Name = "catapimba-corps-vpc"
   }
-}
-
-resource "aws_eip" "jenkins-ip" {
-  instance = module.jenkins_ec2_instance.id
-  vpc      = true
 }
